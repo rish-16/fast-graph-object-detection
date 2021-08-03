0f11971a803544474b61d590a18f52a90ba4a70a
rish-16 <mail.rishabh.anand@gmail.com>
2021-08-03 22:53:57 +0800

    Add adjacency matrix algorithm

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Fast Object Detection with Graph Neural Networks
 Object Detection with Graph Neural Networks is an idea I wanted to experiment with. It treats object detection tasks as node classification tasks where each node is a pixel in the image. 
 
 ## How does it work?
-An image can be considered a grid graph where each pixel is an individual node. A neighbourhood is thus formed with 8 adjacent nodes and a central node. Node features can be the pixel value while node labels can be which object the pixel belongs to (for example, one of the 1000 categories from the MS COCO dataset). Creating this adjacency list is trivial.
+An image can be considered a grid graph where each pixel is an individual node. A Moore Neighbourhood is thus formed with 8 adjacent nodes and a central node. Node features can be the pixel value while node labels can be which object the pixel belongs to (for example, one of the 1000 categories from the MS COCO dataset). Creating this adjacency list is trivial.
 
 Essentially, I reduce object detection to a node classification problem. 
 
@@ -16,10 +16,10 @@ Instead of treating each pixel as a node, I also experiment with image patches. 
 This method does not scale well when the image is very large owing to the number of pixel (nodes) to be trained upon.
 
 ## TODO
-- [ ] Collate data into single source JSON
+- [x] Collate data into single source JSON
 - [ ] Get segmentation masks
 - [ ] Create graph dataset
-    - [ ] Adjacency Matrix
+    - [x] Adjacency Matrix
     - [ ] Node labels based on Convex Hull
     - [ ] Reformat into `torch_geometric` standard
     - [ ] Feed into `torch_geometric.data.DataLoader`
---
+++ src/create_dataset.py
@@ -0,0 +1,73 @@
+import numpy as np
+from einops import rearrange
+import torch
+from pprint import pprint
+
+def display(arr):
+    header = "{:^}\t".format("")
+    for i in range(len(arr)):
+        header += "{:^}\t".format(i)
+    print (header)
+    content = ""
+    for i, entry in enumerate(arr, 0):
+        temp = ""
+        for j in entry:
+            temp += "{:^}\t".format(j)
+        content += "{:^}\t{}\n".format(i, temp)
+        
+    print (content)
+
+def create_adjacency_matrix(img):
+    '''
+    An image has D = HxW pixels. 
+    Each pixel is a node.
+    A pixel's immediate neighbourhood is a the Moore Neighbourhood
+    Adjacency Matrix will be DxD
+    '''
+    def get_neighbourhood(i, j):
+        return [
+            [i, j],
+            [i+1, j],
+            [i-1, j],
+            [i, j+1],
+            [i, j-1],
+            [i+1, j+1],
+            [i-1, j+1],
+            [i+1, j-1],
+            [i-1, j-1],
+        ]
+        
+    h, w, c = img.shape
+    matrix = np.array([np.array([0 for _ in range(h*w)]) for _ in range(h*w)])
+    template = np.array([(i) for i in range(h*w)])
+    template = template.reshape([h, w])
+    
+    def prune_neighbours(arr, h, w):
+        def checker(i, j):
+            return True if (i >= 0 and i < h and j >= 0 and j < w) else False
+        
+        return list(filter(lambda x : checker(x[0], x[1]), arr))
+
+    for i in range(h):
+        for j in range(w):
+            coord = [i, j]
+            source_index = template[i][j]
+            neighbours = get_neighbourhood(i, j)
+            neighbours = prune_neighbours(neighbours, h, w)
+            for rec in neighbours:
+                target_idx = template[rec[0]][rec[1]]
+                matrix[source_index][target_idx] = 1
+                
+    return matrix
+
+def get_node_features(img):
+    h, w, c = img.shape
+    out = rearrange(img, 'h w c -> (h w) c')
+    print (out.shape)
+    return out
+
+img = np.random.randint(0, 10, size=(3, 3, 3))
+print (img)
+print (get_node_features(img))
+# matrix = create_adjacency_matrix(img)
+# display(matrix)
